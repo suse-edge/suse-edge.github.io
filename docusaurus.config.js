ed4932754fca3b79a82d216c8a3f9a5f482cd9dd
Eduardo Mínguez <e.minguez@gmail.com>
2023-04-10 15:46:23 +0200

    Added social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/suse-edge-social-card.jpg',
       navbar: {
         title: 'SUSE Edge docs site',
         logo: {
