917ba073449421da80d7379825d1e138ce8e17dc
Frank Sundermeyer <fs@suse.de>
2024-07-30 14:12:15 +0200

    Switching to a DocBook XML based localization
    because this allows to ignore source code listings

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-./create_po4a.sh asciidoc/edge-book/edge.adoc
-po4a -v --srcdir $(pwd) --destdir $(pwd) -k 0 -M utf-8 -L utf-8 --no-translations -o nolinting edge_po4a.cfg
-for lang in ja ko zh_CN; do mkdir -p asciidoc/l10n-weblate/$lang; cp -axu asciidoc/images/ asciidoc/l10n-weblate/$lang; done
